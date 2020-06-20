d1318a8a155f9f392bda6884722308bf14a80efb
mzx01294516 <kimmazhenxin19900129@163.com>
2020-06-20 23:45:41 +0800

    增加了测试Lombok的测试代码

---
+++ flink-learning-code/src/main/scala/com/kim/tmp/TestLombok.java
@@ -0,0 +1,31 @@
+package com.kim.tmp;
+
+import lombok.Data;
+
+/**
+ * lombok测试使用
+ * @Author: kim
+ * @Date: 2020/6/20 23:34
+ * @Version: 1.0
+ */
+
+public class TestLombok {
+    public static void main(String[] args) {
+        Teacher teacher = new Teacher();
+        teacher.setDesc("优秀");
+        teacher.setId("001");
+        System.out.println(teacher);
+    }
+}
+
+
+
+@Data
+class Teacher {
+    private String id;
+    private String desc;
+
+}
+
+
+
