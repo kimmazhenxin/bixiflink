20dd26e1e1e973bc5792b3809ea261381dfdd282
mazhenxin <mazhenxin@xinye.com>
2021-01-22 10:00:07 +0800

    add SQL Datatype

---
+++ khkw/src/main/java/No46/SQL/Datatypes/datatype/DataTypeSize.java
@@ -0,0 +1,9 @@
+package No46.SQL.Datatypes.datatype;
+
+/**
+ * @Author: mazhenxin
+ * @File: DataTypeSize.java
+ * @Date: 2021/1/21 9:23
+ */
+public class DataTypeSize {
+}
---
+++ khkw/src/main/java/No46/SQL/Datatypes/datatype/StringIsObject.java
@@ -0,0 +1,9 @@
+package No46.SQL.Datatypes.datatype;
+
+/**
+ * @Author: mazhenxin
+ * @File: StringIsObject.java
+ * @Date: 2021/1/21 9:26
+ */
+public class StringIsObject {
+}
