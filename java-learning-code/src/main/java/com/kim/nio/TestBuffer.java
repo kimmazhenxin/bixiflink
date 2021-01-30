5ad08cfd3687357aa68e136da745c9a1076f0cab
kimMa <kimmazhenxin19900129@163.com>
2021-01-30 21:17:56 +0800

    update

---
+++ java-learning-code/src/main/java/com/kim/nio/TestBuffer.java
@@ -0,0 +1,15 @@
+package com.kim.nio;
+
+/**
+ * NIO缓冲区(Buffer)：在 Java NIO 中负责数据的存取。缓冲区就是数组。用于存储不同数据类型的数据
+ * 根据数据类型不同(boolean 除外) ，有以下Buffer 常用子类：ByteBuffer、CharBuffer、ShortBuffer、IntBuffer、LongBuffer、FloatBuffer、DoubleBuffer
+ * @Author: kim
+ * @Date: 2021/1/30 21:11
+ * @Version: 1.0
+ */
+public class TestBuffer {
+
+	public static void main(String[] args) {
+
+	}
+}
